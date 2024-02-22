26b71ed3fadfbfd7b63c16af7f2082bb39e5dde5
lfvalcar <luisfervalcar@outlook.es>
2024-02-22 19:25:57 +0000

    metodo listar todos los libros añadido

--- src/modulos/libros/libros.service.ts
+++ src/modulos/libros/libros.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, InternalServerErrorException} from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateLibroDto } from './dto/create-libro.dto';
-import { UpdateLibroDto } from './dto/update-libro.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Libro } from './entities/libro.entity';
@@ -11,59 +10,65 @@ export class LibrosService {
   constructor(
     @InjectRepository(Libro)
     private readonly librosRepository: Repository<Libro>,
-    private readonly AutoresService: AutoresService
-  ){}
+    private readonly AutoresService: AutoresService,
+  ) {}
 
   async create(createLibroDto: CreateLibroDto) {
     try {
-      const {autor, ...campos} = createLibroDto;
-      const libro = this.librosRepository.create({...campos});
+      const { autor, ...campos } = createLibroDto;
+      const libro = this.librosRepository.create({ ...campos });
       const autorobj = await this.AutoresService.findOne(autor);
       libro.autor = autorobj;
       await this.librosRepository.save(libro);
       return {
         msg: 'Registro Insertado',
         data: libro,
-        status: 200
-      }
-    }catch(error){
-      throw new InternalServerErrorException('Pongase en contacto con el Sysadmin')
+        status: 200,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Pongase en contacto con el Sysadmin',
+      );
     }
   }
 
   findAll() {
-    return `This action returns all libros`;
+    const libros = this.librosRepository.find({
+      relations: {
+        autor: true,
+      },
+    });
+    return libros;
   }
 
   findOne(isbn: string) {
     const autor = this.librosRepository.findOne({
-      where:{
-        isbn
+      where: {
+        isbn,
       },
       relations: {
-        autor: true
-      }
+        autor: true,
+      },
     });
     return autor;
   }
 
-  update(id: number, updateLibroDto: UpdateLibroDto) {
-    return `This action updates a #${id} libro`;
-  }
+  // update(id: number, updateLibroDto: UpdateLibroDto) {
+  //   return `This action updates a #${id} libro`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} libro`;
   }
 
-  async deleteAllLibros(){
+  async deleteAllLibros() {
     const query = this.librosRepository.createQueryBuilder('libro');
-    try{
-      return await query 
-        .delete()
-        .where({})
-        .execute()
-    }catch(error){
-      throw new InternalServerErrorException('Pongase en contacto con el Sysadmin')
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Pongase en contacto con el Sysadmin',
+      );
     }
   }
 }
